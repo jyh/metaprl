81d90a50576a6e4bf08a079f3a716f4d4988efe8
jyh <jyh@5109f7fa-8300-0410-942d-a7a480986ffa>
1998-01-27 23:04:32 +0000

    Adding OCaml1.07 syntax.

--- theories/caml/caml_expr.mli
+++ theories/caml/caml_expr.mli
@@ -1,110 +1,15 @@
 (*
- * OCaml expressions.
+ * "Propositions" about caml programs.
  *)
 
-(*
- * Record projection.
- *)
-declare proj[$start:n; $finish:n]{'e1; 'e2}
+include Caml_program
 
-dform mode[prl] :: proj[$start:n; $finish:n]{'e1; 'e2} =
-   slot{'e1} `"." slot{'e2}
-
-(*
- * Application.
- *)
-declare apply[$start:n; $finish:n]{'e1; 'e2}
-
-dform mode[prl] :: apply[$start:n; $finish:n]{'e1; 'e2} =
-   slot{'e1} space slot{'e2}
-
-(*
- * Array subscript.
- *)
-declare array_subscript[$start:n; $finish:n]{'e1; 'e2}
-
-dform mode[prl] :: array_subscript[$start:n; $finish:n]{'e1; 'e2} =
-   slot{'e1} `".(" slot{'e2} `")"
-
-(*
- * Array construction.
- * Arrays are terminated lists.
- *)
-declare array[$start:n; $finish:n]{'a}
-
-dform mode[prl] :: array[$start:n; $finish:n]{'a} =
-   `"[|" list_cons{'a} `"|]"
-
-(*
- * Assignment.
- *)
-declare assign[$start:n; $finish:n]{'e1; 'e2}
-           
-dform mode[prl] :: assign[$start:n; $finish:n]{'e1; 'e2} =
-   slot{'e1} `":=" slot{'e2}
-
-(*
- * Character constant.
- *)
-declare char[$start:n; $finish:n; $c:s]
-
-(*
- * Class coercion.
- *)
-declare coerce[$start:n; $finish:n]{'e; 't}
-
-dform mode[prl] :: coerce[$start:n; $finish:n]{'e; 't} =
-   slot{'e} `":>" slot{'t}
-
-(*
- * Floating point constant.
- *)
-declare float[$start:n; $finish:n; $f:s]
-
-(*
- * For loop.
- *)
-declare for_upto[$start:n; $finish:n]{'s; 't; v. 'body]
-
-dform mode[prl] :: for_upto[$start:n; $finish:n]{'s; 't; v. 'body] =
-   `"for" slot{'v} `" := " slot{'s} `" to " slot{'t} `" do " sbreak slot{'body} sbreak `"done"
-
-declare for_downto[$start:n; $finish:n]{'s; 't; v. 'body]
-
-dform mode[prl] :: for_downto[$start:n; $finish:n]{'s; 't; v. 'body] =
-   `"for" slot{'v} `" := " slot{'s} `" downto " slot{'t} `" do " sbreak slot{'body} sbreak `"done"
-
-(*
- * Function.
- * Argument is a pwe.
- *)
-declare lambda_pattern[$start:n; $finish:n]{'a}
-
-dform mode[prl] :: lambda_pattern[$start:n; $finish:n]{'a} =
-   df_pwe{'a}
-
-(*
- * Conditional.
- *)
-declare if[$start:n; $finish:n]{'e1; 'e2; 'e3}
-          
-dform mode[prl] :: if[$start:n; $finish:n]{'e1; 'e2; 'e3} =
-   `"if" slot{'e1} `" then" break slot{'e2} break `"else" break slot{'e3}
-                     
-(*
- * Integer constant.
- *)
-declare int[$start:n; $finish:n; $i:n]
-
-dform mode[prl] :: int[$start:n; $finish:n; $i:n] =
-   int[$i:n]
-
-(*
- * Let binding.
- *)
-declare letrec{
+axiom proj : 
 (*
  * $Log$
+ * Revision 1.2  1998/01/27 23:04:28  jyh
+ * Adding OCaml1.07 syntax.
+ *
  * Revision 1.1  1998/01/03 23:20:25  jyh
  * Upgraded to OCaml 1.07, initial semantics of OCaml.
  *
---
+++ theories/caml/caml_syntax.mli
@@ -0,0 +1,164 @@
+(*
+ * OCaml expressions.
+ *)
+declare proj{'e1; 'e2}
+declare apply{'e1; 'e2}
+declare array_subscript{'e1; 'e2}
+declare "array"{'a}
+declare assign{'e1; 'e2}
+declare "char"[$c:s]
+declare coerce{'e; 't}
+declare "float"[$f:s]
+declare for_upto{'s; 't; v. 'body]
+declare for_downto{'s; 't; v. 'body]
+declare lambda_pattern{'a}
+declare "if"{'e1; 'e2; 'e3}
+declare "int"[$i:n]
+declare letrec{'e; 'body}
+declare "let"{'e; 'body}
+declare "match"{'e; 'body}
+declare "new"{'e}
+declare stream{'sel}
+declare record{'el}
+declare sequence{'el}
+declare select{'e; 'i}
+declare string_subscript{'e; 'i}
+declare "try"{'e; 'pwel}
+declare tuple{'el}
+declare cast{'e; 't}
+declare "while"{'e; 'body}
+
+(*
+ * Patterns.
+ *)
+declare patt_proj{'p1; 'p2}
+declare patt_as{'p1; 'p2}
+declare patt_wildcard{}
+declare patt_apply{'p1; 'p2}
+declare patt_char[$s:n]
+declare patt_int[$i:n]
+declare patt_lid[$i:n]
+declare patt_uid[$i:n]
+declare patt_choice{'p1; 'p2}
+declare patt_range{'p1; 'p2}
+declare patt_record{'pl}
+declare patt_string[$s:s]
+declare patt_tuple{'pl}
+declare patt_cast{'p; 't}
+
+(*
+ * Types.
+ *)
+declare type_proj{'t1; 't2}
+declare type_as{'t1; 't2}
+declare type_wildcard
+declare type_apply{'t1; 't2}
+declare type_fun{'t1; 't2}
+declare type_class_id{'t}
+declare type_lid[$v:v]
+declare type_param[$s:s]
+declare type_equal{'t1; 't2}
+declare type_object_tt{'stl}
+declare type_object_ff{'stl}
+declare type_record{'tl}
+declare type_list{'t}
+declare type_prod{'t1; 't2}
+declare type_uid[$v:v]
+
+(*
+ * Signature items.
+ *)
+declare sig_class{'ctl}
+declare sig_exception{'tl}
+declare sig_external{'t; 'sl}
+declare sig_module{'mt}
+declare sig_module_type{'mt}
+declare sig_open{'path}
+declare sig_type{'ssltl}
+declare sig_value{'t}
+
+(*
+ * Structure items.
+ *)
+declare str_class{'cdl}
+declare str_exception{'stl}
+declare str_expr{'e}
+declare str_external{'t; 'sl}
+declare str_module{'me}
+declare str_module_type{'mt}
+declare str_open{'sl}
+declare str_type{'ssltl}
+declare str_let{'pel}
+declare str_letrec{'pel}
+
+(*
+ * Module type.
+ *)
+declare mt_proj{'mt1;'mt2}
+declare mt_apply{'mt1; 'mt2}
+declare mt_functor{'mt1; v. 'mt2}
+declare mt_lid[$v:v]
+declare mt_sig{'sil}
+declare mt_uid[$v:v]
+declare mt_type_with{'mt; 'wcl}
+
+(*
+ * With clause.
+ *)
+declare wc_type{'sl1; 'sl2; 't}
+declare wc_module{'sl1; 'mt}
+
+(*
+ * Module expression.
+ *)
+declare me_proj{'me1; 'me2}
+declare me_apply{'me1; 'me2}
+declare me_functor{'mt; v. 'me}
+declare me_lid[$v:v]
+declare me_struct{'sil}
+declare me_cast{'me; 'mt}
+declare me_uid[$v:v]
+
+(*
+ * Class type.
+ *)
+declare class_type{'sl; 't; 'so; 'ctfl; 'b1; 'b2}
+
+(*
+ * Class type field.
+ *)
+declare class_type_ctr{'s; 't}
+declare class_type_inh{'t}
+declare class_type_mth{'s; 't}
+declare class_type_val{'s; 'b1; 'b2; 'to}
+declare class_type_vir{'s; 't}
+
+(*
+ * Class.
+ *)
+declare class{'sl1; 'pl1; 'so1; 'so2; 'cfl; 'b1; 'b2}
+
+(*
+ * Class field.
+ *)
+declare class_ctr{'s; 't}
+declare class_inh{'t; 'e; 'so}
+declare class_mth{'s; 'e}
+declare class_val{'s; 'b1; 'b2; 'eo}
+declare class_vir{'s; 't}
+
+(*
+ * $Log$
+ * Revision 1.1  1998/01/27 23:04:32  jyh
+ * Adding OCaml1.07 syntax.
+ *
+ * Revision 1.1  1998/01/03 23:20:25  jyh
+ * Upgraded to OCaml 1.07, initial semantics of OCaml.
+ *
+ *
+ * -*-
+ * Local Variables:
+ * Caml-master: "refiner"
+ * End:
+ * -*-
+ *)

3551984813d6063cda48f731cb37db7b9b33a56b
xiny <xiny@5109f7fa-8300-0410-942d-a7a480986ffa>
2002-01-30 02:01:20 +0000

    1. Adopted a new representation for cyclic groups to avoid confusion. 2. Added the definition of cyclic groups. 3. Added the properties of cyclic groups.

--- theories/czf/czf_itt_cyclic_group.ml
+++ theories/czf/czf_itt_cyclic_group.ml
@@ -26,12 +26,31 @@ open Base_auto_tactic
 
 declare cyclic_group{elem_in_G}
 
+prim_rw unfold_cyclic_group : cyclic_group{elem_in_G} <-->
+   cyclic_subgroup{elem_in_G}
+
 dform cyclic_group_df : except_mode[src] :: cyclic_group{elem_in_G} =
-   `"<a>"
+   `"<gen>" subc
 
 (* axioms *)
-interactive cyclic_group_wf1 {| intro [] |} 'H :
-   sequent ['ext] { 'H >- isset{cyclic_group{elem_in_G}} }
-
-interactive cyclic_group_wf2 {| intro [] |} 'H :
+interactive cyclic_group_wf {| intro [] |} 'H :
    sequent ['ext] { 'H >- equal{cyclic_group{elem_in_G}; car} }
+
+(* properties of cyclic groups *)
+interactive cyclic_group_wf1 {| intro [] |} 'H :
+   sequent ['ext] { 'H >-  isset{cyclic_group{elem_in_G}} }
+
+interactive cyclic_group_op_wf {| intro[] |} 'H :
+   sequent [squash] { 'H >- isset{'s1} } -->
+   sequent [squash] { 'H >- isset{'s2} } -->
+   sequent ['ext] { 'H >- mem{'s1; cyclic_group{elem_in_G}} } -->
+   sequent ['ext] { 'H >- mem{'s2; cyclic_group{elem_in_G}} } -->
+   sequent ['ext] { 'H >- mem{op{'s1; 's2}; cyclic_group{elem_in_G}} }
+
+interactive cyclic_group_id_wf {| intro[] |} 'H :
+   sequent ['ext] { 'H >- mem{id; cyclic_group{elem_in_G}} }
+
+interactive cyclic_group_inv_wf {| intro[] |} 'H :
+   sequent [squash] { 'H >- isset{'s} } -->
+   sequent ['ext] { 'H >- mem{'s; cyclic_group{elem_in_G}} } -->
+   sequent ['ext] { 'H >- mem{inv{'s}; cyclic_group{elem_in_G}} }

4d26eefccf622b182300defa85ff27caaf221146
xiny <xiny@5109f7fa-8300-0410-942d-a7a480986ffa>
2002-03-24 07:43:47 +0000

    Changed the definition of abelian groups completely. An abelian group is now defined under equivalence relations instead of equality relations, and there is a reduction rule for it.

--- theories/czf/czf_itt_abel_group.ml
+++ theories/czf/czf_itt_abel_group.ml
@@ -1,24 +1,48 @@
+include Czf_itt_set
+include Czf_itt_member
 include Czf_itt_group
+include Czf_itt_equiv
 
-open Refiner.Refiner
-open Simple_print
+open Printf
+open Mp_debug
+open Refiner.Refiner.TermType
+open Refiner.Refiner.Term
+open Refiner.Refiner.TermOp
+open Refiner.Refiner.TermAddr
+open Refiner.Refiner.TermMan
+open Refiner.Refiner.TermSubst
+open Refiner.Refiner.Refine
+open Refiner.Refiner.RefineError
+open Mp_resource
+
+open Tactic_type
+open Tactic_type.Tacticals
+open Tactic_type.Sequent
+open Tactic_type.Conversionals
+open Mptop
 open Var
 
 open Base_dtactic
 open Base_auto_tactic
 
-declare abel{'g}
+declare abel{'g; 'r}
 
-dform abel_df : except_mode[src] :: abel{'g} =
-   `"abel(" slot{'g} `")"
+prim_rw unfold_abel: abel{'g; 'r} <-->
+   (group{'g} & isset{'r} & equiv{car{'g}; 'r} & (all a: set. all b: set. (mem{'a; car{'g}} => mem{'b; car{'g}} => equiv{car{'g}; 'r; op{'g; 'a; 'b}; op{'g; 'b; 'a}})))
 
-(* axioms *)
-interactive abel_wf {| intro[] |} 'H 's1 's2:
+dform abel_df : except_mode[src] :: abel{'g; 'r} =
+   `"abel(" slot{'g} `"; " slot{'r} `")"
+
+interactive abel_type {| intro [] |} 'H :
+   sequent [squash] { 'H >- 'g IN label } -->
+   sequent [squash] { 'H >- isset{'r} } -->
+   sequent ['ext] { 'H >- group{'g} } -->
+   sequent ['ext] { 'H >- "type"{abel{'g; 'r}} }
+
+interactive abel_intro {| intro[] |} 'H :
    sequent [squash] { 'H >- 'g IN label } -->
    sequent ['ext] { 'H >- group{'g} } -->
-   sequent [squash] { 'H >- isset{'s1} } -->
-   sequent [squash] { 'H >- isset{'s2} } -->
-   sequent ['ext] { 'H >- mem{'s1; car{'g}} } -->
-   sequent ['ext] { 'H >- mem{'s2; car{'g}} } -->
-   sequent ['ext] { 'H >- equal{op{'g; 's1; 's2}; op{'g; 's2; 's1}} } -->
-   sequent ['ext] { 'H >- abel{'g} }
+   sequent [squash] { 'H >- isset{'r} } -->
+   sequent ['ext] { 'H >- equiv{car{'g}; 'r} } -->
+   sequent ['ext] { 'H; a: set; b: set; x: mem{'a; car{'g}}; y: mem{'b; car{'g}} >- equiv{car{'g}; 'r; op{'g; 'a; 'b}; op{'g; 'b; 'a}} } -->
+   sequent ['ext] { 'H >- abel{'g; 'r} }
--- theories/czf/czf_itt_abel_group.mli
+++ theories/czf/czf_itt_abel_group.mli
@@ -1,3 +1,31 @@
+include Czf_itt_set
+include Czf_itt_member
 include Czf_itt_group
+include Czf_itt_equiv
 
-declare abel{'g}
+open Printf
+open Mp_debug
+open Refiner.Refiner.TermType
+open Refiner.Refiner.Term
+open Refiner.Refiner.TermOp
+open Refiner.Refiner.TermAddr
+open Refiner.Refiner.TermMan
+open Refiner.Refiner.TermSubst
+open Refiner.Refiner.Refine
+open Refiner.Refiner.RefineError
+open Mp_resource
+
+open Tactic_type
+open Tactic_type.Tacticals
+open Tactic_type.Sequent
+open Tactic_type.Conversionals
+open Mptop
+open Var
+
+open Base_dtactic
+open Base_auto_tactic
+
+declare abel{'g; 'r}
+
+rewrite unfold_abel: abel{'g; 'r} <-->
+   (group{'g} & isset{'r} & equiv{car{'g}; 'r} & (all a: set. all b: set. (mem{'a; car{'g}} => mem{'b; car{'g}} => equiv{car{'g}; 'r; op{'g; 'a; 'b}; op{'g; 'b; 'a}})))

a2465b47d8874bf8a2f82ab9c636780a3fd5018f
jyh <jyh@5109f7fa-8300-0410-942d-a7a480986ffa>
2002-09-04 19:48:26 +0000

    Updated the intro some.  Can you all take a look and see what you think?

--- theories/ocaml_doc/ocaml_doc_intro.ml
+++ theories/ocaml_doc/ocaml_doc_intro.ml
@@ -47,10 +47,76 @@ Classic ML language designed by Robin Milner in 1975 for the LCF
 (@emph{Logic of Computable Functions}) theorem prover
 @cite["lcf,GMW79"].
 
-Like other dialects of ML, OCaml includes an expressive polymorphic
-type system, type inference, pattern matching, and automatic storage
-management.  In addition, OCaml is the only widely-available dialect
-of ML that has an object system.
+OCaml shares many features with other dialects of ML, and it provides
+several new features of its own.  Throughout this document, we use the
+term ML to stand for any of the dialects of ML, and OCaml when a
+feature is specific to OCaml.
+
+@begin[itemize]
+@item{{ML is a @bf{functional} language, meaning that functions are
+treated as first-class values.  Functions may be nested, functions may
+be passed as arguments to other functions, and functions can be stored
+in data-structures.  Functions are treated like their mathematical
+counterparts as much as possible.  Assignment statements that
+permanently change the value of an expression are permitted, but used
+much less frequently than in languages like C or Java.}}
+
+@item{{
+ML is @bf{strongly typed}, meaning that the type of every variable
+and every expression in a program is determined at compile-time.
+Programs that pass the type checker are @emph{safe}: they will never
+``go wrong'' because of an illegal instruction or memory fault.}}
+
+@item{{
+Related to strong typing, ML uses @bf{type inference} to infer
+types for the expressions in a program.  Even though the language is
+strongly typed, it is rare that the programmer ever has to annotate a
+program with type constraints.}}
+
+@item{{
+The ML type system is @bf{polymorphic}, meaning that it is possible
+to write programs that work for values of any type.  For example, it
+is straightforward to define data structures like lists, stacks, and
+trees that can contain elements of any type.  In a language like C or
+Java, the programmer would either have to write different
+implementations for each type (say, lists of integers vs. lists of
+floating-point values), or else use explicit coercions to bypass the
+type system.}}
+
+@item{{
+ML implements an @bf{pattern matching} mechanism that
+unifies case analysis and data destructors.}}
+
+@item{{
+ML includes an expressive @bf{module system} that allows data
+structures to be specified and defined @emph{abstractly}.  The module
+system includes @emph{functors}, which are are functions over modules
+that can be used to produce one data structure from another.}}
+
+@item{{
+OCaml also the only widely-available ML implementation to include an
+@bf{object system}.  The module system and object system complement
+one another: the module system provides data abstraction, and the
+object system provides inheritance and re-use.}}
+
+@item{{
+OCaml includes a compiler that supports @bf{separate compilation}.
+This makes that the development process easier by reducing the amount
+of code that must be recompiled when a program is modified.  OCaml
+actually includes two compilers: a @emph{byte-code} compiler that
+produces code for the portable OCaml byte-code interpreter, and an
+@emph{native-code} compiler that produces efficient code for many
+machine architectures.}}
+
+@item{{
+One other feature should be mentioned: all the languages in the ML
+family have a @bf{formal semantics}, which means that programs have a
+mathematical interpretation.  While this may not seem to be directly
+useful to a programmer, it means that the programming language
+constructs are designed to fit together, making the programming
+language easier to understand and explain.}}
+
+@end[itemize]
 
 @section[ocaml_doc_intro_functional]{Functional and imperative languages}
 
@@ -104,22 +170,34 @@ Functional programs have some nice properties: one is that data
 structures are @emph{persistent} (by definition), which means that no
 data structure is ever destroyed.
 
-There are problems with taking too strong a stance in favor
-of functional programming.  One is that every updatable data structure
+There are problems with taking too strong a stance in favor of
+functional programming.  One is that every updatable data structure
 has to be passed as an argument to every function that uses it (this
 is called ``threading'' the state).  This can make the code obscure if
 there are too many of these data structures.  We take a moderate
-approach.  Imperative code may be used where it is natural, but
-imperative code @emph{may not} be used if there is a more elegant
+approach.  Imperative code may be used where it is natural, but the
+use of imperative code is discouraged if there is a more elegant
 functional version.
 
 @section[ocaml_doc_intro_organization]{Organization}
 
 This document is organized as a @emph{user guide} to programming in
 OCaml.  It is not a reference manual: there is already an online
-reference manual.  I assume that you have programming experience in an
-imperative language like C; I'll point out the differences between ML
-and C in the cases that seem appropriate.
+reference manual.  I assume that the reader already has some
+experience using an imperative programming language like C; I'll point
+out the differences between ML and C in the cases that seem
+appropriate.
+
+@section[ocaml_doc_intro_additional_source]{Additional Sources of Information}
+
+This document was originally used for a course in compiler
+construction at Caltech.  The course material, including exercises, is
+available at @tt{http://www.cs.caltech.edu/courses/cs134/cs134b}.
+
+The OCaml reference manual @cite["leroy:ocaml"] is available on the
+OCaml home page @tt{http://www.ocaml.org/}.
+
+The author can be reached at @tt{jyh@cs.caltech.edu}.
 
  * @end[doc]
  *)

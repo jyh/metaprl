289262a4a38342f15be081cbeb43a138fe09448a
granicz <granicz@5109f7fa-8300-0410-942d-a7a480986ffa>
2002-09-13 08:05:17 +0000

    Phobos_theory.mlz is now split up.

---
+++ theories/phobos/phobos_theory.ml
@@ -0,0 +1,89 @@
+(*!
+ * @spelling{MCC Mojave}
+ *
+ * @begin[doc]
+ * @theory{Phobos Theory}
+ * @module[Phobos_theory]
+ *
+ * The Phobos theory supplies rewrites needed for Phobos in the 
+ * Mojave compiler (MCC).
+ *
+ * (Documentation incomplete.)
+ *
+ * @end[doc]
+ *
+ * ----------------------------------------------------------------
+ *
+ * @begin[license]
+ * This file is part of MetaPRL, a modular, higher order
+ * logical framework that provides a logical programming
+ * environment for OCaml and other languages.
+ *
+ * See the file doc/index.html for information on Nuprl,
+ * OCaml, and more information about this system.
+ *
+ * Copyright (C) 2002 Adam Granicz, Caltech
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Author: Adam Granicz
+ * @email{granicz@cs.caltech.edu}
+ * @end[license]
+ *)
+
+(*!
+ * @begin[doc]
+ * @parents
+ * @end[doc]
+ *)
+extends Base_meta
+
+extends Itt_atom
+extends Itt_int_base
+extends Itt_int_ext
+extends Itt_list
+extends Itt_list2
+extends Itt_rfun
+
+extends Phobos_base
+
+(*
+ * Reduce meta_num to a number.
+ *)
+prim_rw reduce_meta_num : Base_meta!meta_num[n:n] <--> Itt_int_base!number[n:n]
+
+(*
+ * Add Base_meta rewrites to reduceC.
+ *)
+let resource reduce +=
+   [<<param_add_string[p:s]{'term}>>, Phobos_base.reduce_param_add_string;
+    <<error[msg:s]>>,       Phobos_base.reduce_error0;
+    <<meta_num[a:n]>>,      reduce_meta_num;
+    <<meta_sum[a:n,b:n]>>,  Base_meta.reduce_meta_sum;
+    <<meta_diff[a:n,b:n]>>, Base_meta.reduce_meta_diff;
+    <<meta_prod[a:n,b:n]>>, Base_meta.reduce_meta_prod;
+    <<meta_quot[a:n,b:n]>>, Base_meta.reduce_meta_quot;
+    <<meta_rem[a:n,b:n]>>,  Base_meta.reduce_meta_rem;
+    <<meta_eq[a:n,b:n]{'tt; 'ff}>>, Base_meta.reduce_meta_eq_num;
+    <<meta_eq[a:s,b:s]{'tt; 'ff}>>, Base_meta.reduce_meta_eq_str;
+    <<meta_eq[a:v,b:v]{'tt; 'ff}>>, Base_meta.reduce_meta_eq_var;
+    <<meta_eq[a:t,b:t]{'tt; 'ff}>>, Base_meta.reduce_meta_eq_tok;
+    <<meta_eq[a:l,b:l]{'tt; 'ff}>>, Base_meta.reduce_meta_eq_lev;
+    <<meta_lt[a:n,b:n]{'tt; 'ff}>>, Base_meta.reduce_meta_lt_num;
+    <<meta_lt[a:s,b:s]{'tt; 'ff}>>, Base_meta.reduce_meta_lt_str;
+    <<meta_lt[a:t,b:t]{'tt; 'ff}>>, Base_meta.reduce_meta_lt_tok;
+    <<meta_lt[a:l,b:l]{'tt; 'ff}>>, Base_meta.reduce_meta_lt_lev ]
+
+(*! @docoff *)
---
+++ theories/phobos/phobos_theory.mli
@@ -0,0 +1,62 @@
+(*!
+ * @spelling{MCC Mojave}
+ *
+ * @begin[doc]
+ * @theory{Phobos Theory}
+ * @module[Phobos_theory]
+ *
+ * The Phobos theory supplies rewrites needed for Phobos in the 
+ * Mojave compiler (MCC).
+ *
+ * (Documentation incomplete.)
+ *
+ * @end[doc]
+ *
+ * ----------------------------------------------------------------
+ *
+ * @begin[license]
+ * This file is part of MetaPRL, a modular, higher order
+ * logical framework that provides a logical programming
+ * environment for OCaml and other languages.
+ *
+ * See the file doc/index.html for information on Nuprl,
+ * OCaml, and more information about this system.
+ *
+ * Copyright (C) 2002 Adam Granicz, Caltech
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Author: Adam Granicz
+ * @email{granicz@cs.caltech.edu}
+ * @end[license]
+ *)
+
+(*!
+ * @begin[doc]
+ * @parents
+ * @end[doc]
+ *)
+extends Base_meta
+
+extends Itt_atom
+extends Itt_int_base
+extends Itt_int_ext
+extends Itt_list
+extends Itt_list2
+extends Itt_rfun
+
+extends Phobos_base
+
+(*! @docoff *)

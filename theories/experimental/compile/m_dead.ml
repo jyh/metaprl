c834e07a3eb73573cb2aa5debce8818a35e9eea5
emre <emre@5109f7fa-8300-0410-942d-a7a480986ffa>
2003-04-03 06:42:22 +0000

    Wrote a little documentation for dead code elimination.  Mostly, this is just clean-up.  Is 'redeces' the plural form of 'redex'? If it isn't, that'll need to be fixed here.

--- theories/experimental/compile/m_dead.ml
+++ theories/experimental/compile/m_dead.ml
@@ -1,37 +1,42 @@
-doc <:doc< 
+doc <:doc<
+   @begin[spelling]
+   redeces
+   @end[spelling]
+
    @begin[doc]
-   @module[Dead]
-  
-   This simple form of dead-code elimination is really easy.
-   For any let-definition, let v = a in e, remove the
-   let-definition if v is not free in e.
+   @module[M_dead]
+
+   This module implements an aggressive form of dead-code elimination.
+   A let-definition is considered dead if the variable it defines is not
+   used.  If the defining value would normally raise an exception (e.g.,
+   division by zero), the semantics of the program could change.
    @end[doc]
-  
+
    ----------------------------------------------------------------
-  
+
    @begin[license]
    Copyright (C) 2003 Jason Hickey, Caltech
-  
+
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
    as published by the Free Software Foundation; either version 2
    of the License, or (at your option) any later version.
-  
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-  
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-  
+
    Author: Jason Hickey
    @email{jyh@cs.caltech.edu}
    @end[license]
 >>
 
-doc <:doc< 
+doc <:doc<
    @begin[doc]
    @parents
    @end[doc]
@@ -55,22 +60,20 @@ open Tactic_type.Sequent
  * Resource.
  *)
 
-doc <:doc< 
+doc <:doc<
    @begin[doc]
    @resources
-  
-   @bf{The @Comment!resource[dead_resource]}
-  
+
    The @tt{dead} resource provides a generic method for
    defining @emph{dead code elimination}.  The @conv[deadC] conversion
    can be used to apply this evaluator.
-  
+
    The implementation of the @tt{dead_resource} and the @tt[deadC]
    conversion rely on tables to store the shape of redices, together with the
    conversions for the reduction.
-  
-   @docoff
+
    @end[doc]
+   @docoff
 >>
 let resource dead =
    table_resource_info identity extract_data
@@ -87,9 +90,13 @@ let deadC =
  * Rewrites.
  *)
 
-doc <:doc< 
+doc <:doc<
    @begin[doc]
-   @modsubsection{Dead-code rewrites}
+   @rewrites
+
+   The rewrites are straightforward.  Note that in the redeces below, $v$ is
+   not allowed to be free in $e$.  Each of these rewrites is added to the
+   @tt{dead_resource}.
    @end[doc]
 >>
 prim_rw dead_let_atom :
@@ -102,9 +109,9 @@ prim_rw dead_let_subscript :
    LetSubscript{'a1; 'a2; v. 'e} <--> 'e
 
 prim_rw dead_let_closure :
-   LetClosure{'a1; 'a2; f. 'e} <--> 'e
+   LetClosure{'a1; 'a2; v. 'e} <--> 'e
 
-(* @docoff *)
+doc <:doc< @docoff >>
 
 (*
  * Add all these rules to the dead resource.
@@ -113,7 +120,7 @@ let resource dead +=
     [<< LetAtom{'a; v. 'e} >>, dead_let_atom;
      << LetTuple{'length; 'tuple; v. 'e} >>, dead_let_tuple;
      << LetSubscript{'a1; 'a2; v. 'e} >>, dead_let_subscript;
-     << LetClosure{'a1; 'a2; f. 'e} >>, dead_let_closure]
+     << LetClosure{'a1; 'a2; v. 'e} >>, dead_let_closure]
 
 (*
  * Top-level tactic.

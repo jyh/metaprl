9f5dc3edb9bf13be2255241202daa6d651708475
emre <emre@5109f7fa-8300-0410-942d-a7a480986ffa>
2003-04-03 23:56:18 +0000

    A little more work on trying to clean-up / add documentation. I need to take a look at the assembly language some more; the delete_null_reserve rewrite confuses me right now.

--- theories/experimental/compile/m_x86_opt.ml
+++ theories/experimental/compile/m_x86_opt.ml
@@ -1,35 +1,43 @@
-doc <:doc< 
-   @begin[spelling]
-   CPS ra
-   @end[spelling]
-  
-   Some easy assembly optimizations.
-  
+doc <:doc<
+   @begin[doc]
+   @module[M_x86_opt]
+
+   This module implements some easy assembly optimizations, including
+   dead instruction elimination and removal of null reserves.
+   @end[doc]
+
    ----------------------------------------------------------------
-  
+
    @begin[license]
    Copyright (C) 2003 Jason Hickey, Caltech
-  
+
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
    as published by the Free Software Foundation; either version 2
    of the License, or (at your option) any later version.
-  
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-  
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-  
+
    Author: Jason Hickey
    @email{jyh@cs.caltech.edu}
    @end[license]
 >>
+
+doc <:doc<
+   @begin[doc]
+   @parents
+   @end[doc]
+>>
 extends M_x86_asm
 extends M_util
+doc <:doc< @docoff >>
 
 open M_util
 
@@ -44,23 +52,24 @@ open Tactic_type.Sequent
 open Top_conversionals
 
 (************************************************************************
- * REDUCTION RESOURCES                                                   *
- ************************************************************************)
+ * Resources.
+ *)
 
-doc <:doc< 
+doc <:doc<
    @begin[doc]
    @resources
-  
-   @bf{The @Comment!resource[before_ra_resource]}
-  
-   The @tt{before_ra} resource provides a generic method for
-   defining @emph{CPS transformation}.  The @conv[before_raC] conversion
-   can be used to apply this evaluator.
-  
-   The implementation of the @tt{before_ra_resource} and the @tt[before_raC]
+
+   The @tt[before_ra] resource provides a generic method for defining
+   rewrites that may be applied before register allocation.  The
+   @conv[before_raC] conversion can be used to apply this evaluator.
+
+   The implementation of the @tt[before_ra_resource] and the @tt[before_raC]
    conversion rely on tables to store the shape of redices, together with the
    conversions for the reduction.
-  
+
+   The @tt[after_ra] resource and corresponding conversion @tt[after_raC]
+   are similar.
+
    @docoff
    @end[doc]
 >>
@@ -90,9 +99,19 @@ let after_raC =
  * Optimizations for before (and after) register allocation.
  *)
 
-(*
- * Dead instruction elimination.
- *)
+doc <:doc<
+   @begin[doc]
+   @rewrites
+   @modsubsection{Dead instruction elimination}
+
+   Dead instructions, i.e. those instructions that define a variable that is
+   not used in the rest of the program, may be eliminated.  The rewrites
+   below are aggressive;  the program's semantics could change if an
+   instruction that can raise an exception is eliminated.  These rewrites
+   are added to the @tt[before_ra] resource, although they may be applied
+   after register allocation as well.
+   @end[doc]
+>>
 prim_rw dead_mov :
    Mov{'src; dst. 'e}
    <-->
@@ -127,9 +146,15 @@ prim_rw dead_set :
  * Optimizations that are only valid after register allocation.
  *)
 
-(*
- * Remove reserves of 0 bytes.
- *)
+doc <:doc<
+   @begin[doc]
+   @modsubsection{Null reserve elimination}
+
+   Null reserves may be eliminated from the program.  The rewrite below is
+   added to the @tt[after_ra] resource since it is valid only after register
+   allocation.
+   @end[doc]
+>>
 prim_rw delete_null_reserve :
    Cmp["cmp"]{'a1; 'a2;
    Jcc[opcode:s]{'cc; AsmReserve[0:n]{'params};

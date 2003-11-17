c1f77f2d377564b471dba4bc142b712c446dfde1
kopylov <kopylov@5109f7fa-8300-0410-942d-a7a480986ffa>
2003-11-17 07:46:55 +0000

    Fixed some display forms.

--- theories/itt/itt_comment.ml
+++ theories/itt/itt_comment.ml
@@ -1,29 +1,29 @@
-doc <:doc< 
+doc <:doc<
    @begin[doc]
    @module[Itt_comment]
-   
+
    Terms used for comments in the @Nuprl type theory.
    @end[doc]
-   
+
    ----------------------------------------------------------------
-   
+
    @begin[license]
    Copyright (C) 2000 Jason Hickey, Caltech
-   
+
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
    as published by the Free Software Foundation; either version 2
    of the License, or (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-   
+
    Author: Jason Hickey
    @email{jyh@cs.caltech.edu}
    @end[license]
@@ -823,11 +823,11 @@ declare math_squash{'A}
  *)
 
 dform math_set_df1 : mode[tex] :: math_set{'x; 'A; 'B} =
-   izone `"\\left\\{" ezone
+   izone `"\\{" ezone
    slot{'x}
    izone `"\\colon " ezone
    slot{'A} `"|" slot{'B}
-   izone `"\\right\\}" ezone
+   izone `"\\}" ezone
 
 dform math_squash_df1 : mode[tex] :: math_squash{'A} =
    izone `"\\sq{" ezone

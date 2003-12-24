34d6d7f833867ce412999c7c0a3e7418d4e16ab5
kopylov <kopylov@5109f7fa-8300-0410-942d-a7a480986ffa>
2003-12-24 01:26:20 +0000

    I have theory of relation structures of that includes preorders and equality relation. I need it for rbtrees. Currently this theory is a little bit mess and I did not prove anything.

--- theories/itt/itt_relation_str.ml
+++ theories/itt/itt_relation_str.ml
@@ -1,43 +1,43 @@
-doc <:doc< 
+doc <:doc<
    @begin[doc]
    @module[Itt_relation_str]
-  
+
    The @tt[Itt_relation_str] module defines algebraic structures such as
    ordered sets, @misspelled{PERs}, types with decidable equality and so on.
    @end[doc]
-  
+
    ----------------------------------------------------------------
-  
+
    @begin[license]
    This file is part of MetaPRL, a modular, higher order
    logical framework that provides a logical programming
    environment for OCaml and other languages.
-  
+
    See the file doc/index.html for information on Nuprl,
    OCaml, and more information about this system.
-  
+
    Copyright (C) 1998 Jason Hickey, Cornell University
-  
+
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
    as published by the Free Software Foundation; either version 2
    of the License, or (at your option) any later version.
-  
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-  
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-  
+
    Author: Alexei Kopylov
    @email{apk6@cs.cornell.edu}
    @end[license]
 >>
 
-doc <:doc< 
+doc <:doc<
    @begin[doc]
    @parents
    @end[doc]
@@ -53,8 +53,16 @@ extends Itt_comment
 doc <:doc< @docoff >>
 
 open Tactic_type.Tacticals
+open Tactic_type
 open Var
 open Top_conversionals
+open Itt_struct
+open Refiner.Refiner.Term
+open Refiner.Refiner.TermOp
+open Refiner.Refiner.TermMan
+open Refiner.Refiner.TermSubst
+open Refiner.Refiner.RefineError
+open Typeinf
 
 open Dtactic
 
@@ -72,7 +80,7 @@ let reduceByRecDefC term unfold = reduceByDefC unfold thenC higherC (makeFoldC t
 let soft_reduce term unfold  = term, (reduceByDefC unfold)
 let softrec_reduce term unfold  = term, (reduceByRecDefC term unfold)
 
-doc <:doc< 
+doc <:doc<
    @begin[doc]
    @modsection{Order}
    @modsubsection{Definition}
@@ -82,13 +90,14 @@ doc <:doc<
    @end[doc]
 >>
 
-define order1 : order[i:l] <-->
+define preorder : preorder[i:l] <-->
     { car : univ[i:l];
-      "<" : ^car -> ^car -> bool ;
-      all x:^car. not{"assert"{.'x ^< 'x}};
-      all x:^car.all y:^car.all z:^car. ("assert"{.'x ^< 'y} &  "assert"{.'y ^< 'z} =>  "assert"{.'x ^< 'z});
-      all x:^car.all y:^car. ("assert"{.'x ^< 'y} or  "assert"{.'y ^< 'x} or 'x='y in ^car)
+      "<=" : ^car -> ^car -> bool ;
+      all x:^car. "assert"{.'x ^<= 'x};
+      all x:^car.all y:^car.all z:^car. ("assert"{.'x ^<= 'y} &  "assert"{.'y ^<= 'z} =>  "assert"{.'x ^<= 'z});
+      all x:^car.all y:^car. ("assert"{.'x ^<= 'y} or  "assert"{.'y ^<= 'x})
     }
+
 
 (*
 define irreflexiveOrder1 :  IrreflexiveOrder[i:l] <--> { self : orderSig[i:l] | all x:^car. not{"assert"{.'x ^< 'x}}}
@@ -106,12 +115,20 @@ define order1 : order[i:l] <-->  { self : PartialOrder[i:l] | all x:^car.all y:^
 
 doc <:doc< @docoff >>
 
-define less: less{'self; 'a;'b} <--> "assert"{.'a ^< 'b}
+define le: le{'self; 'a;'b} <--> "assert"{.'b ^<= 'a}
+
+define eq: eq{'self; 'a;'b} <--> le{'self; 'a;'b} and le{'self; 'b; 'a}
+
+define less: less{'self; 'a;'b} <--> le{'self; 'a; 'b} and not{eq{'self;'a;'b}}
+
+
+
+let resource reduce += <<less{rcrd[x:t]{'f;'r};'a;'b}>> , (less thenC addrC [0;0;0] reduceTopC)
 
 dform less_df : parens :: except_mode[src] :: less{'self; 'a;'b}
  = 'a  bf[" <"]sub{'self} `" " 'b
 
-doc <:doc< 
+doc <:doc<
    @begin[doc]
    @modsection{Decidable Equality}
    @modsubsection{Definition}
@@ -128,7 +145,7 @@ define decEquality : DecEquality[i:l] <-->
 
 doc <:doc< @docoff >>
 
-dform order_df : except_mode[src] :: order[i:l] = `"Order" sub{slot[i:l]}
+dform preorder_df : except_mode[src] :: preorder[i:l] = `"preOrder" sub{slot[i:l]}
 
 dform deq_df : except_mode[src] ::  DecEquality[i:l] = `"DecEquality" sub{slot[i:l]}
 
@@ -138,52 +155,169 @@ let irreflexiveOrder = irreflexiveOrder1 thenC higherC fold_less
 
 let transitiveOrder = transitiveOrder1 thenC higherC fold_less
 *)
-let order = order1 thenC higherC fold_less
+let order = preorder thenC higherC fold_less
 
-let resource elim += soft_elim <<order[i:l]>> order
+let resource elim += soft_elim <<preorder[i:l]>> order
 
 doc "doc"{rules}
 
-interactive less_wf  {| intro[] |} order[i:l] :
-   [wf] sequent { <H> >- 'ord in order[i:l] }  -->
+interactive le_wf  {| intro[intro_typeinf <<'ord>>] |} preorder[i:l] :
+   [wf] sequent { <H> >- 'ord in preorder[i:l] }  -->
    [wf] sequent { <H> >- 'x in 'ord^car }  -->
    [wf] sequent { <H> >- 'y in 'ord^car }  -->
-   sequent { <H> >- 'x <['ord] 'y in bool}
+   sequent { <H> >- 'x <=['ord] 'y in bool}
 
-define compare: compare{'self; 'a;'b; 'less_case; 'equal_case; 'greater_case} <--> if 'a ^< 'b then 'less_case else if 'b ^< 'a then 'greater_case else 'equal_case
-
-interactive three_cases  compare{'ord; 'x;'y; 'less_case; 'equal_case; 'greater_case}  order[i:l]  bind{t.'T['t]}:
-   [wf] sequent { <H> >- 'ord in order[i:l] }  -->
+interactive less_prop_wf  {| intro[intro_typeinf <<'ord>>] |} preorder[i:l] :
+   [wf] sequent { <H> >- 'ord in preorder[i:l] }  -->
    [wf] sequent { <H> >- 'x in 'ord^car }  -->
    [wf] sequent { <H> >- 'y in 'ord^car }  -->
-   sequent { <H>; u:  less{'ord;'x;'y} >- 'T['less_case] }  -->
-   sequent { <H>; u:  less{'ord;'y;'x} >- 'T['greater_case] }  -->
-   sequent { <H>; u:  'x = 'y in 'ord^car >- 'T['equal_case] }  -->
-   sequent { <H> >- 'T[compare{'ord; 'x;'y; 'less_case; 'equal_case; 'greater_case}]}
+   sequent { <H> >- "type"{less{'ord;'x; 'y}} }
+
+interactive car_wf  {| intro[intro_typeinf <<'ord>>] |} preorder[i:l] :
+   [wf] sequent { <H> >- 'ord in preorder[i:l] }  -->
+   sequent { <H> >- "type"{'ord^car} }
+
+interactive car_univ  {| intro[intro_typeinf <<'ord>>] |} preorder[i:l] :
+   [wf] sequent { <H> >- 'ord in preorder[i:l] }  -->
+   sequent { <H> >- 'ord^car in univ[i:l] }
+
+interactive eq_prop_wf  {| intro[intro_typeinf <<'ord>>] |} preorder[i:l] :
+   [wf] sequent { <H> >- 'ord in preorder[i:l] }  -->
+   [wf] sequent { <H> >- 'x in 'ord^car }  -->
+   [wf] sequent { <H> >- 'y in 'ord^car }  -->
+   sequent { <H> >- "type"{eq{'ord;'x; 'y}} }
+
+interactive eq_refl  {| intro[intro_typeinf <<'ord>>] |} preorder[i:l] :
+   [wf] sequent { <H> >- 'ord in preorder[i:l] }  -->
+   [wf] sequent { <H> >- 'x in 'ord^car }  -->
+   sequent { <H> >- eq{'ord;'x; 'x} }
+
+define compare: compare{'self; 'a;'b; 'less_case; 'equal_case; 'greater_case} <--> if 'a ^<= 'b then  if 'b ^<= 'a then 'equal_case else 'less_case else  'greater_case
+
+interactive_rw compare_less:
+   less{'ord;'x;'y} -->
+   compare{'ord; 'x;'y; 'less_case; 'equal_case; 'greater_case} <--> 'less_case
+
+interactive_rw compare_equal:
+   eq{'ord; 'x; 'y} -->
+   compare{'ord; 'x;'y; 'less_case; 'equal_case; 'greater_case} <--> 'equal_case
+
+interactive_rw compare_greater:
+   less{'ord;'y;'x} -->
+   compare{'ord; 'x;'y; 'less_case; 'equal_case; 'greater_case} <--> 'greater_case
+
+interactive three_cases  ('x = 'y in 'ord^car) preorder[i:l]:
+   [wf] sequent { <H> >- 'ord in preorder[i:l] }  -->
+   [wf] sequent { <H> >- 'x in 'ord^car }  -->
+   [wf] sequent { <H> >- 'y in 'ord^car }  -->
+   [less] sequent { <H>; u:  less{'ord;'x;'y} >- 'T }  -->
+   [equal] sequent { <H>; u:  eq{'ord; 'x; 'y} >- 'T }  -->
+   [greater] sequent { <H>; u:  less{'ord;'y;'x} >- 'T }  -->
+   sequent { <H> >- 'T}
 
 doc docoff
 
-let decideOrder3T compare_term order_term = funT (fun p ->
-   let bind = get_bind_from_arg_or_concl_subst p compare_term in
-      three_cases compare_term order_term bind)
+let decideOrder3T compare_term order_term =
+      three_cases compare_term order_term thenLT
+         [idT; idT; idT;
+          rwhAll (compare_less thenTC nthHypT (-1));
+          rwhAll (compare_equal thenTC nthHypT (-1));
+          rwhAll (compare_greater thenTC nthHypT (-1))]
+
 
 doc <:doc< @doc{ } >>
 
-interactive compare_wf {| intro [] |} order[i:l] :
-   [wf] sequent { <H> >- 'ord in order[i:l] }  -->
+
+interactive compare_wf {| intro [intro_typeinf <<'ord>>] |} preorder[i:l] :
+   [wf] sequent { <H> >- 'ord in preorder[i:l] }  -->
    [wf] sequent { <H> >- 'x in 'ord^car }  -->
    [wf] sequent { <H> >- 'y in 'ord^car }  -->
    sequent { <H>; u:  less{'ord;'x;'y} >- 'less_case in 'T }  -->
    sequent { <H>; u:  less{'ord;'y;'x} >- 'greater_case in 'T } -->
-   sequent { <H>; u: 'x = 'y in 'ord^car >- 'equal_case in 'T } -->
+   sequent { <H>; u: eq{'ord;'x;'y} >- 'equal_case in 'T } -->
    sequent { <H> >-  compare{'ord; 'x;'y; 'less_case; 'equal_case; 'greater_case} in 'T}
 
-dform match_tree_df : except_mode[src] :: compare{'O; 'a;'b; 'less_case; 'equal_case; 'greater_case} =
+dform compare_df : except_mode[src] :: compare{'O; 'a;'b; 'less_case; 'equal_case; 'greater_case} =
    szone pushm[0] pushm[3] `"Compare in " slot{'O} `": " hspace
        'a `"<" 'b `" -> " slot{'less_case} hspace
        'a `"=" 'b `" -> " slot{'equal_case}  hspace
        'a `">" 'b `" -> " slot{'greater_case} popm popm ezone
 
+
+interactive trans_less_less preorder[i:l] 'H:
+   [wf] sequent { <H> >- 'ord in preorder[i:l] }  -->
+   [wf] sequent { <H> >- 'x in 'ord^car }  -->
+   [wf] sequent { <H> >- 'y in 'ord^car }  -->
+   [wf] sequent { <H> >- 'z in 'ord^car }  -->
+   sequent { <H>; less{'ord;'x;'z}   >- 'C } -->
+   sequent { <H>; less{'ord;'x;'y}; less{'ord;'y;'z}  >- 'C}
+
+interactive trans_less_eq preorder[i:l] 'H:
+   [wf] sequent { <H>  >- 'ord in preorder[i:l] }  -->
+   [wf] sequent { <H> >- 'x in 'ord^car }  -->
+   [wf] sequent { <H> >- 'y in 'ord^car }  -->
+   [wf] sequent { <H> >- 'z in 'ord^car }  -->
+   sequent { <H>; less{'ord;'x;'z}   >- 'C } -->
+   sequent { <H>; less{'ord;'x;'y}; eq{'ord;'y;'z} >- 'C}
+
+interactive trans_eq_less preorder[i:l] 'H:
+   [wf] sequent { <H> >- 'ord in preorder[i:l] }  -->
+   [wf] sequent { <H> >- 'x in 'ord^car }  -->
+   [wf] sequent { <H> >- 'y in 'ord^car }  -->
+   [wf] sequent { <H> >- 'z in 'ord^car }  -->
+   sequent { <H>; less{'ord;'x;'z}   >- 'C } -->
+   sequent { <H>; eq{'ord;'x;'y}; less{'ord;'y;'z} >- 'C}
+
+interactive trans_less_less0 preorder[i:l] 'H:
+   [wf] sequent { <H> >- 'ord in preorder[i:l] }  -->
+   [wf] sequent { <H> >- 'x in 'ord^car }  -->
+   [wf] sequent { <H> >- 'y in 'ord^car }  -->
+   [wf] sequent { <H> >- 'z in 'ord^car }  -->
+   sequent { <H> >- less{'ord;'y;'z}  } -->
+   sequent { <H>; less{'ord;'x;'y} >- less{'ord;'x;'z} }
+
+interactive trans_less_less1 preorder[i:l] 'H:
+   [wf] sequent { <H> >- 'ord in preorder[i:l] }  -->
+   [wf] sequent { <H> >- 'x in 'ord^car }  -->
+   [wf] sequent { <H> >- 'y in 'ord^car }  -->
+   [wf] sequent { <H> >- 'z in 'ord^car }  -->
+   sequent { <H> >- less{'ord;'x;'y}  } -->
+   sequent { <H>; less{'ord;'y;'z} >- less{'ord;'x;'z} }
+
+interactive sym_eq_eq preorder[i:l] 'H:
+   [wf] sequent { <H> >- 'ord in preorder[i:l] }  -->
+   [wf] sequent { <H> >- 'x in 'ord^car }  -->
+   [wf] sequent { <H> >- 'y in 'ord^car }  -->
+   sequent { <H>; eq{'ord;'x;'y} >- eq{'ord;'y;'x} }
+
+
+(* BUG: shoild be implemented using resource(s) *)
+let some_transT n level = firstT [trans_less_less level n; trans_less_eq level n; trans_eq_less level n]
+
+let some_transT0 n level = firstT [trans_less_less0 level n; trans_less_less1 level n; sym_eq_eq level n]
+
+declare order[i:l]
+
+
+let hypTransT n m =
+   funT (fun p ->
+            let term1 = Sequent.nth_hyp p n in
+            let ord = match explode_term term1 with
+                  <<less{'ord;'x;'y}>> | <<eq{'ord;'x;'y}>> -> ord |
+                  _ -> raise (RefineError ("hypTransT", StringTermError("transitivity does not know about ",term1))) in
+            let level = infer_type p ord in
+               if n=0 || m=0 then
+                  let k = n+m in copyHypT k (-1) thenT some_transT0 (-1) level
+               else
+                  copyHypT n (-1) thenT copyHypT m (-1) thenT some_transT (-2) level
+        )
+
+
+interactive irref_less_eq {| elim[elim_typeinf <<'ord>> ] |} preorder[i:l] 'H :
+   [wf] sequent { <H> >- 'ord in preorder[i:l] }  -->
+   sequent { <H>; less{'ord;'x;'x}; <J> >- 'C }
+
+(*
 doc <:doc< @begin[doc]
   Corollary: The equality is decidable in ordered sets
   @end[doc]
@@ -194,12 +328,69 @@ interactive dec_equalaty  order[i:l] :
    sequent { <H> >- 'x in 'ord^car }  -->
    sequent { <H> >- 'y in 'ord^car }  -->
    sequent { <H> >-  decidable{.'x='y in 'ord^car} }
+*)
+
+
+doc <:doc< @begin[doc]
+@modsection{Product}
+  @end[doc]
+>>
+
+define type_porduct_ord: type_product_ord{'T;'Ord} <-->
+   {car='T * 'Ord^car;
+    "<=" = lambda {a.lambda {b. snd{'a} <=['Ord] snd{'b}}}
+   }
+
+let resource reduce +=
+   <<field[f:t]{ type_product_ord{'T;'Ord} }>>, (addrC [0] type_porduct_ord thenC reduceTopC)
+
+interactive_rw compare_type_porduct_ord_reduce {| reduce |}:
+   compare{type_product_ord{'T;'Ord} ; ('a,'x);('b,'y); 'less_case; 'equal_case; 'greater_case} <-->
+   compare{'Ord ; 'x;'y; 'less_case; 'equal_case; 'greater_case}
+
+interactive type_porduct_ord_preorder {| intro[] |}:
+   sequent { <H> >- 'T in univ[i:l] } -->
+   sequent { <H> >- 'Ord in preorder[i:l] } -->
+   sequent { <H> >-  type_product_ord{'T;'Ord} in preorder[i:l] }
+
+
+dform  type_porduct_ord_df: type_product_ord{'T;'Ord} = ('T * 'Ord)
 
 doc <:doc< @begin[doc]
 @modsection{Example: integers}
   @end[doc]
 >>
 
-define int_order: int_order <--> {car= int; "<"= lambda{a.lambda{b.lt_bool{'a;'b}}}}
+define int_order: int_order <--> {car= int; "<"= lambda{a.lambda{b.lt_bool{'a;'b}}};"<="= lambda{a.lambda{b.le_bool{'a;'b}}}}
+
+
+
+let resource reduce +=
+   <<compare{int_order ; number[n:n];number[m:n]; 'less_case; 'equal_case; 'greater_case}>>, (addrC [0] int_order thenC compare)
+
 
 doc docoff
+
+
+let preorder_opname = opname_of_term <<preorder[i:l]>>
+let dest_preorder = dest_univ_term preorder_opname
+let mk_preorder_term = mk_univ_term preorder_opname
+let type_product_ord_opname = opname_of_term <<type_product_ord{'T;'Ord}>>
+let dest_type_product_ord = dest_dep0_dep0_term type_product_ord_opname
+
+(*
+let infer_preorder inf consts decls eqs opt_eqs defs t =
+   let a, b = dest_type_product_ord t in
+   let eqs', opt_eqs', defs', a' = inf consts decls eqs opt_eqs defs a in
+   let eqs'', opt_eqs'', defs'', b' = inf consts decls eqs' opt_eqs' defs' b in
+   let le1 = Itt_equal.dest_univ a' in
+   let le2 = dest_preorder b' in
+      eqs'', opt_eqs'', defs'', mk_preorder_term (max_level_exp le1 le2 0)
+*)
+
+let infer_preorder inf consts decls eqs opt_eqs defs t =
+   let a, b = dest_type_product_ord t in
+      inf consts decls eqs opt_eqs defs b
+
+
+let resource typeinf +=  <<type_product_ord{'T;'Ord}>>,  infer_preorder
--- theories/itt/itt_relation_str.mli
+++ theories/itt/itt_relation_str.mli
@@ -11,6 +11,7 @@ declare TransitiveOrder[i:l]
 
 declare PartialOrder[i:l]
 *)
+declare preorder[i:l]
 declare order[i:l]
 
 declare DecEquality[i:l]
@@ -18,7 +19,12 @@ declare DecEquality[i:l]
 declare compare{'self; 'a;'b; 'less_case; 'equal_case; 'greater_case}
 
 declare less{'self; 'a;'b}
+declare eq{'self; 'a;'b}
 
 declare int_order
 
 topval decideOrder3T : term -> term -> tactic
+
+topval  hypTransT  : int -> int -> tactic
+
+declare type_product_ord{'T;'Ord}

dabd8440398bb6577c543b08038e516bddd5a569
nogin <nogin@5109f7fa-8300-0410-942d-a7a480986ffa>
2004-01-27 07:08:13 +0000

    - The Itt_antiquotient.eq_mem_eq rule was missing an assumption, fixed. - Fixed a few broken links in htmlman - Started a theory implementing a "base" type idea that I've posted to the newsgroup about a month ago.

--- theories/itt/itt_antiquotient.ml
+++ theories/itt/itt_antiquotient.ml
@@ -1,41 +1,41 @@
-doc <:doc< 
+doc <:doc<
    @begin[doc]
    @module[Itt_antiquotient]
-  
+
    See @cite[Nog02c], Section 6 for explanations.
-  
+
    ----------------------------------------------------------------
-  
+
    @begin[license]
    This file is part of MetaPRL, a modular, higher order
    logical framework that provides a logical programming
    environment for OCaml and other languages.
-  
+
    See the file doc/index.html for information on Nuprl,
    OCaml, and more information about this system.
-  
-   Copyright (C) 1998 Jason Hickey, Cornell University
-  
+
+   Copyright (C) 2002-2004 MetaPRL Group
+
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
    as published by the Free Software Foundation; either version 2
    of the License, or (at your option) any later version.
-  
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-  
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-  
+
    Author: Aleksey Nogin @email{nogin@cs.cornell.edu}
    @end[license]
    @end[doc]
 >>
 
-doc <:doc< 
+doc <:doc<
    @begin[doc]
    @parents
    @end[doc]
@@ -46,7 +46,7 @@ extends Itt_bisect
 extends Itt_quotient
 extends Itt_ext_equal
 
-doc <:doc< 
+doc <:doc<
    @begin[doc]
    The following rule says that a type is uniquely determined
    by its equality relation.
@@ -54,14 +54,15 @@ doc <:doc<
 >>
 
 prim eq_mem_eq 'H :
+   [wf] sequent{ <H> >- 'X Type } -->
    sequent{ <H>; x1: 'X; x2: 'X >- ('x1 = 'x2 in 'A) => ('x1 = 'x2 in 'B)} -->
-   sequent{ <H>; x: 'A; y: esquash{('x in 'X)} >- 'x in 'B } = it
+   sequent{ <H>; x: 'A; esquash{('x in 'X)} >- 'x in 'B } = it
 
 interactive antiquotient univ[i:l] :
    sequent{ <H> >- \subtype{'A; 'B} } -->
    sequent{ <H> >- \subtype{'B; . quot x,y: 'A // "true" }} -->
    (* We know A Type, but need explicit univ number *)
-   sequent{ <H> >- 'A in univ[i:l] } -->
+   [wf] sequent{ <H> >- 'A in univ[i:l] } -->
    sequent{ <H> >- \subtype{'B ; . quot u,v: 'A // ('u='v in 'B)}}
 
 interactive quotent_isect univ[i:l] :
@@ -73,3 +74,5 @@ interactive quotent_isect univ[i:l] :
    [wf] sequent{ <H>; x: 'A; y: 'A >- "type"{'E2['x; 'y]} } -->
    sequent{ <H> >- ext_equal{bisect{(quot x,y : 'A //'E1['x;'y]); (quot x,y : 'A //'E2['x;'y])};
                                   (quot x,y : 'A //('E1['x;'y] & 'E2['x;'y])) }}
+
+doc docoff
---
+++ theories/itt/itt_eq_base.ml
@@ -0,0 +1,101 @@
+doc <:doc<
+   @begin[doc]
+   @module[Itt_eq_base]
+
+   The @tt{Itt_eq_base} module defines a $base$ type that allows turning
+   squiggle equality into a first-class equality relation.
+   @end[doc]
+
+   For more information, see also the newsgroup thread at
+   news://news.metaprl.org/1ad4.3fd93890.24661%40news.metaprl.org
+   (Message posted "Thu, 11 Dec 2003 19:39:59 -0800" by Aleksey Nogin
+    with Subject ``Idea: "Base" type that solves a lot of problems'').
+
+   ----------------------------------------------------------------
+
+   @begin[license]
+
+   This file is part of MetaPRL, a modular, higher order
+   logical framework that provides a logical programming
+   environment for OCaml and other languages.
+
+   See the file doc/index.html for information on Nuprl,
+   OCaml, and more information about this system.
+
+   Copyright (C) 2004 MetaPRL group.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   Author: Aleksey Nogin @email{nogin@cs.cornell.edu}
+
+   @end[license]
+>>
+
+open Dtactic
+
+doc <:doc<
+   @begin[doc]
+   @parents
+
+   The following modules are needed to express the basic properties of the
+   constructors presented here.
+   @end[doc]
+>>
+extends Itt_equal
+extends Itt_squiggle
+extends Itt_esquash
+
+doc <:doc<
+   @doc{
+      The following modules are only needed to be able to prove extenede properties
+      of these constructors.
+   }
+>>
+extends Itt_isect
+extends Itt_quotient
+extends Itt_ext_equal
+extends Itt_logic
+extends Itt_antiquotient
+
+doc "doc"{terms}
+declare base
+doc docoff
+
+dform base_df : except_mode[src] :: base = `"Base"
+
+doc "doc"{rules}
+
+prim base_univ {| intro [] |} :
+   sequent { <H> >- base in univ[i:l] } = it
+
+interactive base_wf {| intro [] |} :
+   sequent { <H> >- base Type }
+
+prim base_sqeq :
+   sequent { <H> >- esquash {'x = 'y in base} } -->
+   sequent { <H> >- ('x ~ 'y) } = it
+
+interactive_rw base_rw ('x ~ 'y) :
+   esquash {'x = 'y in base} -->
+   'x <--> 'y
+
+prim base_eq :
+   sequent { <H> >- ('x ~ 'y) } -->
+   sequent { <H> >- esquash {'x = 'y in base} } = it
+
+interactive base_top {| intro [] |} :
+   sequent { <H> >- ext_equal {top; quot x,y:base // "true"} }
+
+doc docoff

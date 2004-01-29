5419bc7d2ff1b23e5691b3a67582b15b33efe08c
xiny <xiny@5109f7fa-8300-0410-942d-a7a480986ffa>
2004-01-29 09:37:29 +0000

    Forgot this file.

---
+++ theories/itt/itt_unitringCE.mli
@@ -0,0 +1,67 @@
+(*
+ * Commutative unitrings with decidable equality.
+ *
+ * ----------------------------------------------------------------
+ *
+ * This file is part of MetaPRL, a modular, higher order
+ * logical framework that provides a logical programming
+ * environment for OCaml and other languages.
+ *
+ * See the file doc/index.html for information on Nuprl,
+ * OCaml, and more information about this system.
+ *
+ * Copyright (C) 1997-2004 MetaPRL Group
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Author: Xin Yu
+ * Email : xiny@cs.caltech.edu
+ *)
+
+extends Itt_unitring
+extends Itt_ringE
+
+open Tactic_type.Conversionals
+
+(************************************************************************
+ * SYNTAX                                                               *
+ ************************************************************************)
+
+declare preunitringE[i:l]
+declare isUnitRingCE{'f}
+declare unitringCE[i:l]
+
+(************************************************************************
+ * TACTICS                                                              *
+ ************************************************************************)
+
+topval unfold_preunitringE : conv
+topval unfold_isUnitRingCE : conv
+topval unfold_unitringCE : conv
+
+topval fold_preunitringE1 : conv
+topval fold_preunitringE : conv
+topval fold_isUnitRingCE1 : conv
+topval fold_isUnitRingCE : conv
+topval fold_unitringCE1 : conv
+topval fold_unitringCE : conv
+
+(*
+ * -*-
+ * Local Variables:
+ * Caml-master: "editor.run"
+ * End:
+ * -*-
+ *)

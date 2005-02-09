ea24ea69131b343b0cc4c451bba84aee12bd0ed6
nogin <nogin@5109f7fa-8300-0410-942d-a7a480986ffa>
2005-02-09 01:44:11 +0000

    Adding couple of missing declarations.

--- theories/itt/itt_synt_bterm.mli
+++ theories/itt/itt_synt_bterm.mli
@@ -6,3 +6,5 @@ declare make_bterm{'op; 'subterms}
 declare bterm_ind{'bt; v.'var_case['v];
                        op,subterms,ind. 'op_case['op; 'subterms; 'ind] }
 declare compatible_shapes{'op; 'btl}
+declare dest_bterm{'bt; v.'var_case['v]; op,subterms. 'op_case['op; 'subterms] }
+declare bdepth{'bt}

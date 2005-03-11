f2a3eee7ccb0d44b82848502dfd7c9ce42b472e0
kopylov <kopylov@5109f7fa-8300-0410-942d-a7a480986ffa>
2005-03-11 02:01:08 +0000

    Added a definition of beta-reduction for the simple lambda-calculus

--- theories/itt/itt_reflection_example_lambda.mli
+++ theories/itt/itt_reflection_example_lambda.mli
@@ -1,1 +1,3 @@
-
+declare LambdaTerm
+declare mk_apply{'t;'s}
+declare mk_lambda{'t}
---
+++ theories/itt/itt_reflection_lambda_reduction.ml
@@ -0,0 +1,40 @@
+(* This theory define beta-reduction for the simple lambda-calculus *)
+extends Itt_reflection_new
+extends Itt_synt_subst
+extends Itt_reflection_example_lambda
+
+open Basic_tactics
+
+define unfold_subst2: subst{'f;'b} <--> subst{add_vars_upto{'f;'b};last_var{'f};'b}
+
+interactive subst_wf {| intro [] |} :
+   sequent { <H> >- 'f in BTerm } -->
+   sequent { <H> >- 'b in BTerm } -->
+   sequent { <H> >- subst{'f;'b} in BTerm }
+
+
+define unfold_beta_redex: beta_redex{'redex;'contractum} <-->
+   exst f:LambdaTerm. exst b:LambdaTerm.
+    ('redex = mk_apply{ mk_lambda{'f}; 'b} in LambdaTerm &
+     'contractum = subst{'f;'b} in LambdaTerm)
+
+interactive beta_redex_wf {| intro[] |}:
+   sequent{ <H> >- 't in LambdaTerm } -->
+   sequent{ <H> >- 's in LambdaTerm } -->
+   sequent{ <H> >- beta_redex{'t;'s} Type }
+
+interactive beta_redex_bterm:
+   sequent{ <H> >- beta_redex{bterm{| <K> >- apply[@]{lambda[@]{x.'f['x]}; 'b} |}; bterm{| <K> >- 'f['b] |} } }
+
+define unfold_reduce1: reduce1{'t;'s} <-->
+   exst f:LambdaTerm. exst redex:LambdaTerm. exst contractum:LambdaTerm.
+    ('t = subst{'f;'redex} in LambdaTerm &
+     's = subst{'f;'contractum} in LambdaTerm &
+     beta_redex{'redex;'contractum})
+
+interactive beta_reduce1_wf {| intro[] |}:
+   sequent{ <H> >- 't in LambdaTerm } -->
+   sequent{ <H> >- 's in LambdaTerm } -->
+   sequent{ <H> >- reduce1{'t;'s} Type }
+
+

08f8e7f96e6ea02def6809f44dfa6f12220e0d5d
yegor <yegor@5109f7fa-8300-0410-942d-a7a480986ffa>
2005-06-24 22:55:23 +0000

    Forgot to commit it for consistency - I changed the name of a probably unused variable, provided as a string parameter to J-prover.

--- theories/itt/itt_logic.ml
+++ theories/itt/itt_logic.ml
@@ -1363,7 +1363,7 @@ let base_jproverT def_mult = funT (fun p ->
       ITT_JProver.prover mult_limit hyps seq.sequent_concl
    with
       [t] ->
-         let substs = try [Lm_symbol.add "v0_jprover", get_with_arg p] with RefineError _ -> [] in
+         let substs = try [Lm_symbol.add "vnewj0", get_with_arg p] with RefineError _ -> [] in
             t substs assums
     | _ -> raise (Invalid_argument "Problems decoding ITT_JProver.prover proof"))
 

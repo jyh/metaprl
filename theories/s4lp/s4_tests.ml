6996ddb68a7a884e9275bc64c9356a878db81945
yegor <yegor@5109f7fa-8300-0410-942d-a7a480986ffa>
2005-07-16 01:27:05 +0000

    muddy children are also partially working but the problem here seems to be in performance

--- theories/s4lp/s4_tests.ml
+++ theories/s4lp/s4_tests.ml
@@ -54,4 +54,50 @@ interactive box0_m3 :
  * MUDDY CHILDREN               *
  ********************************)
 
+declare c1
+declare c2
+declare c3
+declare c4
+
+define unfold_mchKAO : mchKAO <-->
+	kwh[1]{c2} & kwh[1]{c3} & kwh[1]{c4} &
+   kwh[2]{c1} & kwh[2]{c3} & kwh[2]{c4} &
+   kwh[3]{c1} & kwh[3]{c2} & kwh[3]{c4} &
+   kwh[4]{c1} & kwh[4]{c2} & kwh[4]{c3}
+
+define unfold_s0 : s0 <--> c1 & c2 & not{c3} & not{c4} & mchKAO
+define unfold_s1 : s1 <--> s0 & box[0]{c1 or c2 or c3 or c4}
+define unfold_s2 : s2 <-->
+	s1 &
+	box[0]{
+		(c1 & c2) or
+      (c1 & c3) or
+      (c1 & c4) or
+      (c2 & c3) or
+      (c2 & c4) or
+      (c3 & c4)
+	}
+
+define unfold_s3 : s3 <-->
+	s2 &
+	box[0]{
+		kwh[1]{c1} &
+		kwh[2]{c2}
+	}
+
+
+let unfold_mchT =
+	byDefsT [unfold_s3; unfold_s2; unfold_s1; unfold_s0; unfold_mchKAO; unfold_kao; unfold_kwh]
+
+interactive lemma3 :
+	sequent { s2 >- concl {| kwh[1]{c1} & kwh[2]{c2} |} }
+
+interactive wrong :
+	sequent { s2 >- concl {| kwh[1]{c1} & kwh[2]{c2} & kwh[3]{c3} & kwh[4]{c4} |} }
+
+interactive lemma4 :
+   sequent { s3 >- concl {| kwh[3]{c3} |} }
+
+interactive muddy_children :
+   sequent { s3 >- concl {| kwh[1]{c1} & kwh[2]{c2} & kwh[3]{c3} & kwh[4]{c4} |} }
 
--- theories/s4lp/s4_tests.mli
+++ theories/s4lp/s4_tests.mli
@@ -2,3 +2,5 @@ open Basic_tactics
 
 topval unfold_wmT : tactic
 topval wmT : tactic
+
+topval unfold_mchT : tactic

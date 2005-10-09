f69f94d04b03854a5c51221aef3a9e1e90c7eadc
jyh <jyh@5109f7fa-8300-0410-942d-a7a480986ffa>
2005-10-09 23:32:24 +0000

    First pass on defining the HOAS model. I think I need a theory of finite functions first.

--- theories/poplmark/naive/pmn_core_hoas_model.ml
+++ theories/poplmark/naive/pmn_core_hoas_model.ml
@@ -27,10 +27,65 @@
 extends Pmn_core_hoas_terms
 
 (************************************************************************
- * Type model.
+ * Type classes.
  *)
 define unfold_tenv : TEnv <--> <:itt<
-    Prod n: int * ({ 0..n- } -> TyExp)
+    Prod n: int * ({ 0..n- } -> soas_TyExp)
+>>
+
+define unfold_venv : VEnv <--> <:itt<
+    Prod n: int * ({ 0..n- } -> soas_Exp)
+>>
+
+prim_rw unfold_ty_var_type : <:itt_rw<
+    TyVar
+    <-->
+    int
+>>
+
+prim_rw unfold_ty_exp : <:itt_rw<
+    TyExp
+    <-->
+    << TEnv >> -> soas_TyExp
+>>
+
+prim_rw unfold_var_type : <:itt_rw<
+    Var
+    <-->
+    int
+>>
+
+prim_rw unfold_exp : <:itt_rw<
+    Exp
+    <-->
+    << VEnv >> -> soas_Exp
+>>
+
+(************************************************************************
+ * Type expressions.
+ *)
+prim_rw unfold_ty_top : <:itt_rw<
+    top
+    <-->
+    fun tenv -> soas_top
+>>
+
+prim_rw unfold_ty_var : <:itt_rw<
+    type { ~v }
+    <-->
+    fun tenv -> tenv v
+>>
+
+prim_rw unfold_ty_fun : <:itt_rw<
+   type { t1 -> t2 }
+   <-->
+   fun tenv -> soas_type { itt { t1 tenv } -> itt { t2 tenv } }
+>>
+
+prim_rw unfold_ty_all : <:itt_rw<
+   type { all X <: t1. t2[X] }
+   <-->
+   fun tenv -> soas_type { all X <: itt { t1 tenv }. (* t2[fst tenv] (tenv with X) *) t2[X] }
 >>
 
 (*!

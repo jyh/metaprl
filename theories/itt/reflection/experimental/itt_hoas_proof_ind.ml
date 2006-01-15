31a8af8df32b13426c236aaaa8d209233bd41fc7
xiny <xiny@5109f7fa-8300-0410-942d-a7a480986ffa>
2006-01-15 09:14:09 +0000

    Added elimination rules which do not do thinning. The one with bindings is hard; not proved yet.

--- theories/itt/reflection/experimental/itt_hoas_proof_ind.ml
+++ theories/itt/reflection/experimental/itt_hoas_proof_ind.ml
@@ -3,8 +3,7 @@ extends Itt_hoas_proof1
 open Dform
 open Basic_tactics
 
-interactive provable_elim 'H :
-   [wf] sequent { <H>; <J> >- 'ty Type } -->
+interactive provable_elim1 'H :
    [wf] sequent { <H>; <J> >- 'logic in Logic } -->
    [wf] sequent { <H>; <J>; v: 'ty >- 'A['v] in BTerm } -->
    sequent { <H>; v: 'ty; <J>;
@@ -18,12 +17,30 @@ interactive provable_elim 'H :
        >- 'C['v] }-->
    sequent { <H>; v: 'ty; Provable{'logic; 'A['v]}; <J> >- 'C['v] }
 
-(*!
- * @docoff
- *
- * -*-
- * Local Variables:
- * Caml-master: "compile"
- * End:
- * -*-
- *)
+interactive provable_elim2 'H :
+   [wf] sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J> >- 'logic in Logic } -->
+   [wf] sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J>; w: 'ty >- 'A['w] in BTerm } -->
+   sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J>;
+       premises: list{Derivation{'logic}};
+       goal: BTerm;
+       witness: ProofStepWitness;
+       ValidStep{'premises; 'goal; 'witness; 'logic};
+       'A['v] = 'goal in BTerm;
+       all_list{'premises; premise. (Provable{'logic; derivation_goal{'premise}} &
+                    all w: 'ty. (('A['w] = derivation_goal{'premise} in BTerm) => 'C['w]))}
+       >- 'C['v] }-->
+   sequent { <H>; v: 'ty; Provable{'logic; 'A['v]}; <J> >- 'C['v] }
+
+interactive provable_elim 'H :
+   [wf] sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J['v; 'x]> >- 'logic in Logic } -->
+   [wf] sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J['v; 'x]>; w: 'ty >- 'A['w] in BTerm } -->
+   sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J['v; 'x]>;
+       premises: list{Derivation{'logic}};
+       goal: BTerm;
+       witness: ProofStepWitness;
+       ValidStep{'premises; 'goal; 'witness; 'logic};
+       'A['v] = 'goal in BTerm;
+       all_list{'premises; premise. (Provable{'logic; derivation_goal{'premise}} &
+                    all w: 'ty. (('A['w] = derivation_goal{'premise} in BTerm) => 'C['w]))}
+       >- 'C['v] }-->
+   sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J['v; 'x]> >- 'C['v] }

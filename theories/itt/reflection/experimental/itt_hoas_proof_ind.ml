6d23a2bbb5da86eb03b14c1f004b8bcf6ad426e7
xiny <xiny@5109f7fa-8300-0410-942d-a7a480986ffa>
2006-01-18 01:49:18 +0000

    Cleaned the theory.

--- theories/itt/reflection/experimental/itt_hoas_proof_ind.ml
+++ theories/itt/reflection/experimental/itt_hoas_proof_ind.ml
@@ -3,38 +3,6 @@ extends Itt_hoas_proof1
 open Dform
 open Basic_tactics
 
-interactive provable_elim1 'H :
-   [wf] sequent { <H>; <J> >- 'logic in Logic } -->
-   [wf] sequent { <H>; <J>; v: 'ty >- 'A['v] in BTerm } -->
-   sequent { <H>; v: 'ty; <J>;
-       premises: list{Derivation{'logic}};
-       goal: BTerm;
-       witness: ProofStepWitness;
-       ValidStep{'premises; 'goal; 'witness; 'logic};
-       'A['v] = 'goal in BTerm;
-       all_list{'premises; premise. (Provable{'logic; derivation_goal{'premise}} &
-                    all w: 'ty. (('A['w] = derivation_goal{'premise} in BTerm) => 'C['w]))}
-       >- 'C['v] }-->
-   sequent { <H>; v: 'ty; Provable{'logic; 'A['v]}; <J> >- 'C['v] }
-
-interactive provable_elim2 'H :
-   [wf] sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J> >- 'logic in Logic } -->
-   [wf] sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J>; w: 'ty >- 'A['w] in BTerm } -->
-   sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J>;
-       premises: list{Derivation{'logic}};
-       goal: BTerm;
-       witness: ProofStepWitness;
-       ValidStep{'premises; 'goal; 'witness; 'logic};
-       'A['v] = 'goal in BTerm;
-       all_list{'premises; premise. (Provable{'logic; derivation_goal{'premise}} &
-                    all w: 'ty. (('A['w] = derivation_goal{'premise} in BTerm) => 'C['w]))}
-       >- 'C['v] }-->
-   sequent { <H>; v: 'ty; Provable{'logic; 'A['v]}; <J> >- 'C['v] }
-
-interactive provable_elim3 'H :
-   sequent { <H>; e: Derivation{'logic}; z: derivation_goal{'e} = 't in BTerm; <J['e, it]> >- 'C['e, it] } -->
-   sequent { <H>; x: Provable{'logic; 't}; <J['x]> >- 'C['x] }
-
 interactive provable_elim 'H :
    [wf] sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J['v; 'x]> >- 'logic in Logic } -->
    [wf] sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J['v; 'x]>; w: 'ty >- 'A['w] in BTerm } -->

c0abb6dd8e4ccb3a2966b258401b86c82ece8254
xiny <xiny@5109f7fa-8300-0410-942d-a7a480986ffa>
2006-02-21 05:40:19 +0000

    I've been trying to prove a better elimination rule. I think provable_elim1 better resembles what we need in pmn_core_terms. However, no matter how I tried, I always came across a well-formedness assumption (w: ty; ... => C[w] Type).
    So is the rule statement wrong? Or is this really what we need?

--- theories/itt/reflection/experimental/itt_hoas_proof_ind.ml
+++ theories/itt/reflection/experimental/itt_hoas_proof_ind.ml
@@ -1,4 +1,6 @@
 extends Itt_hoas_proof
+extends Itt_hoas_sequent_bterm
+extends Itt_hoas_sequent_proof
 
 open Dform
 open Basic_tactics
@@ -9,11 +11,50 @@ interactive provable_elim 'H :
    sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J['v; 'x]>;
        u: 'ty;
        premises: list{Derivation{'logic}};
-       goal: BTerm;
        witness: ProofStepWitness;
-       ValidStep{'premises; 'goal; 'witness; 'logic};
-       'A['u] = 'goal in BTerm;
+       ValidStep{'premises; 'A['u]; 'witness; 'logic};
        all_list{'premises; premise. (Provable{'logic; derivation_goal{'premise}} &
                     all w: 'ty. (('A['w] = derivation_goal{'premise} in BTerm) => 'C['w]))}
        >- 'C['u] }-->
    sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J['v; 'x]> >- 'C['v] }
+
+interactive provableSequent_elim 'H :
+   [wf] sequent { <H>; v: 'ty; x: ProvableSequent{'logic; 'A['v]}; <J['v; 'x]> >- 'logic in Logic } -->
+   [wf] sequent { <H>; v: 'ty; x: ProvableSequent{'logic; 'A['v]}; <J['v; 'x]>; w: 'ty >- 'A['w] in BSequent } -->
+   sequent { <H>; v: 'ty; x: ProvableSequent{'logic; 'A['v]}; <J['v; 'x]>;
+       u: 'ty;
+       premises: list{Derivation{'logic}};
+       witness: ProofStepWitness;
+       ValidStep{'premises; 'A['u]; 'witness; 'logic};
+       all_list{'premises; premise. (Provable{'logic; derivation_goal{'premise}} &
+                    all w: 'ty. (('A['w] = derivation_goal{'premise} in BTerm) => 'C['w]))}
+       >- 'C['u] }-->
+   sequent { <H>; v: 'ty; x: ProvableSequent{'logic; 'A['v]}; <J['v; 'x]> >- 'C['v] }
+
+interactive provable_elim1 'H :
+   [wf] sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J['v; 'x]> >- 'logic in Logic } -->
+   [wf] sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J['v; 'x]>; w: 'ty >- 'A['w] in BTerm } -->
+   sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J['v; 'x]>;
+       u: 'ty;
+       premises: list{BTerm};
+       witness: ProofStepWitness;
+       SimpleStep{'premises; 'A['u]; 'witness; 'logic};
+       all_list{'premises; premise. (Provable{'logic; 'premise} &
+                    all w: 'ty. (('A['w] = 'premise in BTerm) => 'C['w]))}
+       >- 'C['u] }-->
+   sequent { <H>; v: 'ty; x: Provable{'logic; 'A['v]}; <J['v; 'x]> >- 'C['v] }
+
+interactive provableSequent_elim1 'H :
+   [wf] sequent { <H>; v: 'ty; x: ProvableSequent{'logic; 'A['v]}; <J['v; 'x]> >- 'logic in Logic } -->
+   [wf] sequent { <H>; v: 'ty; x: ProvableSequent{'logic; 'A['v]}; <J['v; 'x]>; w: 'ty >- 'A['w] in BSequent } -->
+   sequent { <H>; v: 'ty; x: ProvableSequent{'logic; 'A['v]}; <J['v; 'x]>;
+       u: 'ty;
+       premises: list{BSequent};
+       witness: ProofStepWitness;
+       SimpleStep{'premises; 'A['u]; 'witness; 'logic};
+       (*'A['u] = ...some bsequent ... in BTerm
+       ProvableSequent{'logic; 'A['u]}*)
+       all_list{'premises; premise. (ProvableSequent{'logic; 'premise} &
+                    all w: 'ty. (('A['w] = 'premise in BTerm) => 'C['w]))}
+       >- 'C['u] }-->
+   sequent { <H>; v: 'ty; x: ProvableSequent{'logic; 'A['v]}; <J['v; 'x]> >- 'C['v] }

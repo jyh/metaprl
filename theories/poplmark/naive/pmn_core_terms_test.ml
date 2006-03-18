d7e85d252706a8cad82b1fd4c666e4f9b9e9c32d
xiny <xiny@5109f7fa-8300-0410-942d-a7a480986ffa>
2006-03-18 07:15:45 +0000

    Added a test file.

---
+++ theories/poplmark/naive/pmn_core_terms_test.ml
@@ -0,0 +1,44 @@
+(*
+ * Tests.
+ *
+ * ----------------------------------------------------------------
+ *
+ * @begin[license]
+ * Copyright (C) 2003-2005 Mojave Group, Caltech
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * @end[license]
+ *)
+extends Base_theory
+extends Reflect_pmn_core_terms
+extends Itt_hoas_theory
+
+open Basic_tactics
+open Itt_equal
+open Itt_dfun
+open Itt_logic
+
+interactive tyExp_left 't2 : <:xrule<
+   <H1> >- ProvableSequent{pmn_core_terms; $`meta_type{| <H> >- meta_member{ TyFun{t1; t2}; TyExp{} } |}} -->
+   <H1> >- ProvableSequent{pmn_core_terms; $`meta_type{| <H> >- meta_member{ t1; TyExp{} } |} }
+>>
+
+(*
+ * -*-
+ * Local Variables:
+ * End:
+ * -*-
+ *)
---
+++ theories/poplmark/naive/pmn_core_terms_test.mli
@@ -0,0 +1,4 @@
+extends Base_theory
+extends Reflect_pmn_core_terms
+
+open Basic_tactics

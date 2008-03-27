740c2c229e0705daab81511a73cf930640c69457
yegor <yegor@5109f7fa-8300-0410-942d-a7a480986ffa>
2008-03-27 02:43:07 +0000

    added a wf-lemma and a syllogism rule

--- theories/cic/minimal_logic.ml
+++ theories/cic/minimal_logic.ml
@@ -6,8 +6,19 @@ open Dtactic
 extends Cic_ind_elim_dep
 open Cic_ind_elim_dep
 
+interactive implication_wf {| intro [] |} :
+   sequent { <H> >- 'A in Prop } -->
+   sequent { <H> >- 'B in Prop } -->
+   sequent { <H> >- (x:'A -> 'B) in Prop }
+
 interactive distr_impl :
 	sequent { A : Prop; B : Prop; C : Prop >-  ('A->('B->'C))->(('A->'B)->('A->'C)) }
+
+interactive syllogism :
+   sequent {
+      A : Prop; B : Prop; C : Prop
+      >- ('A -> 'B) -> (('B -> 'C) -> ('A -> 'C))
+   }
 
 extends Cic_prop_connectives
 open Cic_prop_connectives
